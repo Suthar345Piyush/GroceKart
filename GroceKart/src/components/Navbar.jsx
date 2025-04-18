aafeb6bd135d3a7908a60944ed0ddca82a31d6b1
Suthar345Piyush <piyushsuthar524@gmail.com>
2025-04-18 16:32:53 +0530

    Fixed a small problem

--- GroceKart/src/components/Navbar.jsx
+++ GroceKart/src/components/Navbar.jsx
@@ -17,7 +17,7 @@ function Navbar() {
   return (
     <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
 
-    <NavLink to='/'>
+    <NavLink to='/' onClick={() => setOpen(false)}>
         <img className="h-9" src={assets.logo} alt="logo" />
     </NavLink>
 
@@ -29,28 +29,25 @@ function Navbar() {
 
         <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
             <input className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search products" />
-            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M10.836 10.615 15 14.695" stroke="#7A7B7D" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" />
-                <path clip-rule="evenodd" d="M9.141 11.738c2.729-1.136 4.001-4.224 2.841-6.898S7.67.921 4.942 2.057C2.211 3.193.94 6.281 2.1 8.955s4.312 3.92 7.041 2.783" stroke="#7A7B7D" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" />
-            </svg>
+          <img src={assets.search_icon} alt='search' className='w-4 h-4'/>
         </div>
 
-        <div className="relative cursor-pointer">
-            <svg width="18" height="18" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M.583.583h2.333l1.564 7.81a1.17 1.17 0 0 0 1.166.94h5.67a1.17 1.17 0 0 0 1.167-.94l.933-4.893H3.5m2.333 8.75a.583.583 0 1 1-1.167 0 .583.583 0 0 1 1.167 0m6.417 0a.583.583 0 1 1-1.167 0 .583.583 0 0 1 1.167 0" stroke="#615fff" stroke-linecap="round" stroke-linejoin="round" />
-            </svg>
+        <div className="relative cursor-pointer" onClick={() => navigate("/cart")}>
+            <img src={assets.nav_cart_icon} alt='cart' className='w-6 opacity-80'/>
             <button className="absolute -top-2 -right-3 text-xs text-white bg-primary-dull w-[18px] h-[18px] rounded-full">3</button>
         </div>
 
         { !user ? (<button onClick={() => setShowUserLogin(true)} className="cursor-pointer px-8 py-2 bg-primary hover:bg-orange-400 transition text-white rounded-full">
             Login
         </button>) : (
-               <div>
-                 <img src={assets.profile_icon} className="w-10" alt=""/>
-                 <ul>
-                    <li>My Orders</li>
-                    <li>Logout</li>
+               <div className="relative group inline-block">
+                 <img src={assets.profile_icon} className="w-10 cursor-pointer"   alt="Profile"/>
+                 <div className="absolute right-0 mt-2 w-40 bg-white rounded-md shadow-lg opacity-0 group-hover:opacity-100 group-hover:visible invisible transition-opacity duration-300 z-50">
+                 <ul className="py-2 text-sm text-gray-700">
+                    <li onClick={() => navigate("/my-orders")} className="p-1.5 pl-3 hover:bg-primary/10 cursor-pointer">My Orders</li>
+                    <li onClick={logout} className="p-1.5 pl-3 hover:bg-primary/10 cursor-pointer">Logout</li>
                  </ul>
+              </div>
                </div>
         )}
     </div>
